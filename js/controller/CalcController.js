37b4e37437b859dc868b030469f7b377c7ead055
Juan Savedra <juansavedra827@gmail.com>
2022-06-15 14:08:33 -0300

    manipulating the dom

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -2,6 +2,17 @@ class CalcController {
     constructor() {
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "15/06/2022";
+        timeEl.innerHTML = "14:08";
     }
 
     get displayCalc() {
