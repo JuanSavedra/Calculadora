663ac5ee6971a765e3f68f520bdc5f8b1974232d
Juan Savedra <juansavedra827@gmail.com>
2022-06-15 16:41:09 -0300

    adding click event

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -6,13 +6,14 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
         this.setDisplayDateTime();
 
         setInterval(() => {
-            setDisplayDateTime();
+            this.setDisplayDateTime();
         }, 1000);
     }
 
@@ -22,7 +23,13 @@ class CalcController {
     }
 
     initButtonsEvents() {
-        document.querySelectorAll("#buttons > g, #parts > g");
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+        });
     }
 
     get displayTime() {
