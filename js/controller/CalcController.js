c78f7408c82058c56a7c8c7575ed37aaa8e3f397
Juan Savedra <juansavedra827@gmail.com>
2022-06-16 10:52:24 -0300

    validating the first operator and calculating the result

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -54,6 +54,24 @@ class CalcController {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
     }
 
+    pushOperation(value) {
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+            this.calc();
+        }
+    }
+    
+    calc() {
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""))
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay() {
+        
+    }
+
     addOperation(value) {
         if (isNaN(this.getLastOperation())) {
             //String
@@ -61,19 +79,23 @@ class CalcController {
                 //Trocar o operador
                 this.setLastOperation(value);
             } else if (isNaN(value)) {
-                //Verificação do value
-                console.log(value);
-            } else {
                 //Outro valor
-                this._operation.push(value);
+                console.log("Outro valor", value);
+            } else {
+                this.pushOperation(value);
             } 
         } else {
             //Number
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
-        }
+            if (this.isOperator(value)) {
+                this.pushOperation(value);
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
 
-        console.log(this._operation);
+                //Atualizando o display
+                this.setLastNumberToDisplay();
+            }
+        }
     }
 
     executeButton(value) {
